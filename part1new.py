b9f225fd62784e2a2c4d26f36e886ef8caf534bd
Sachin Kumar Prajapati <sachinkp1211@gmail.com>
2019-04-18 23:14:31 +0530

    part1new

--- part1new.py
+++ part1new.py
@@ -52,29 +52,79 @@ pca = load('pca50.joblib')
 ##########################################################################
 #TAKING INPUT OF ALL IMAGES AND REDUCING DIMENSION AT SAME TIME
 
-images_list = []
-reward_list = []
-for folder in foldersfull:
-    for f in sorted(glob.glob(folder+"/*.png")):
-        images_list.append(f)
-    reward_list.append(folder+"/rew.csv")
+# images_list = []
+# reward_list = []
+# for folder in foldersfull:
+#     for f in sorted(glob.glob(folder+"/*.png")):
+#         images_list.append(f)
+#     reward_list.append(folder+"/rew.csv")
 
-Xfull = []
+# Xfull = []
 
-# read_images = []        
-c=0
-for image in images_list:
-    c+=1
-    if(c%2000==0):
-    	print("Input of full data: ", c/2000)
-    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-    temp = np.array(img)
-    temp = temp.flatten()
-    temp = temp.reshape(33600, 1)
-    temp = temp.transpose()
-    temp = pca.transform(temp)
-    temp =temp.reshape(50)
-    Xfull.append(list(temp))
+# # read_images = []        
+# c=0
+# for image in images_list:
+#     c+=1
+#     if(c%2000==0):
+#     	print("Input of full data: ", c/2000)
+#     img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+#     temp = np.array(img)
+#     temp = temp.flatten()
+#     temp = temp.reshape(33600, 1)
+#     temp = temp.transpose()
+#     temp = pca.transform(temp)
+#     temp =temp.reshape(50)
+#     Xfull.append(list(temp))
 
-Xfull = np.array(Xfull)
+# Xfull = np.array(Xfull)
+
+# np.save("dataXfull", Xfull)
+
+# ########### Input Y #########
+# Yfull = []
+
+# for rewards in reward_list:
+# 	temp = []
+# 	with open(rewards) as fileX:
+# 		x_reader = csv.reader(fileX)
+# 		for row in x_reader:
+# 			temp.append(int(float(row[0])))
+# 		Yfull.append(temp)
+
+# Yfull = np.array(Yfull)
+# np.save("dataYfull", Yfull)
+# #############################
+
+#Loading
+Xfull = np.load("dataXfull.npy")
 print(Xfull.shape)
+
+Yfull = np.load("dataYfull.npy")
+print(Yfull.shape)
+
+##########################################################################
+#EXTRA FUNCTIONS
+
+def decision(probability):
+	return random.random() < probability
+
+def tolist(t):
+	temp = [] 
+	for i in range(4):
+		temp.append(t[i])
+	return temp
+
+
+def getcomb(j):
+	result = []
+	a = j-7
+	b = j-2
+	temp = range(a, b+1)
+	temp1 = list(combinations(temp, 4))
+	for i in range(len(temp1)):
+		result.append(list(temp1[i]))
+	return result
+
+##########################################################################
+#GENERATING TRAINING DATA
+
