4141642fd99fad851d537816293bc994bf29ab1b
Sachin Kumar Prajapati <sachinkp1211@gmail.com>
2019-04-22 20:51:58 +0530

    used LinearSVC

--- part1hpc.py
+++ part1hpc.py
@@ -132,97 +132,104 @@ def getcomb(j):
 ##########################################################################
 #GENERATING TRAINING DATA
 
-Xtrain_pos = []
-Ytrain_pos = []
+# Xtrain_pos = []
+# Ytrain_pos = []
 
-Xtrain_neg = []
-Ytrain_neg = []
-
-
-Yindexing = [None]*len(Y)
-Yindexing[0] = 0
-
-for i in range(1, len(Y)):
-	Yindexing[i] = Yindexing[i-1] + len(Y[i-1])+1
-
-print(Yindexing)
+# Xtrain_neg = []
+# Ytrain_neg = []
 
 
-for i in range(len(Yindexing)):
-	num_images = len(Y[i])+1
-	for j in range(7, len(Y[i])):
-		if(Y[i][j]==1):
-			combs = getcomb(j)
-			for k in range(len(combs)):
-				templist = combs[k] 			
-				templist.append(j-1)
-				# print(templist)
-				temptrain = []
-				for l in range(len(templist)):
-					final_index = Yindexing[i] + templist[l]
-					temptrain += list(Xfull[final_index])
-				Xtrain_pos.append(temptrain)
-				Ytrain_neg.append(1)
-		else:
-			if(decision(0.15)):
-				combs = getcomb(j)
-				for k in range(len(combs)):
-					if(decision(0.5)):
-						templist = combs[k] 			
-						templist.append(j-1)
-						# print(templist)
-						temptrain = []
-						for l in range(len(templist)):
-							final_index = Yindexing[i] + templist[l]
-							temptrain += list(Xfull[final_index])
-						Xtrain_neg.append(temptrain)
-						Ytrain_neg.append(Y[i][j])
+# Yindexing = [None]*len(Y)
+# Yindexing[0] = 0
+
+# for i in range(1, len(Y)):
+# 	Yindexing[i] = Yindexing[i-1] + len(Y[i-1])+1
+
+# # print(Yindexing)
 
 
-
-Xtrain = Xtrain_pos[0:10000] + Xtrain_neg[0:10000]
-Ytrain = Ytrain_pos[0:10000] + Ytrain_neg[0:10000]
-np.save("dataXtrain", Xtrain)
-np.save("dataYtrain", Ytrain)
-
-Xtrain = np.load("dataXtrain.npy")
-Ytrain = np.load("dataYtrain.npy")
-print(Xtrain.shape)
-print(Ytrain.shape)
+# for i in range(len(Yindexing)):
+# 	num_images = len(Y[i])+1
+# 	for j in range(7, len(Y[i])):
+# 		if(Y[i][j]==1):
+# 			combs = getcomb(j)
+# 			for k in range(len(combs)):
+# 				templist = combs[k] 			
+# 				templist.append(j-1)
+# 				# print(templist)
+# 				temptrain = []
+# 				for l in range(len(templist)):
+# 					final_index = Yindexing[i] + templist[l]
+# 					temptrain += list(Xfull[final_index])
+# 				Xtrain_pos.append(temptrain)
+# 				Ytrain_pos.append(1)
+# 		else:
+# 			if(decision(0.15)):
+# 				combs = getcomb(j)
+# 				for k in range(len(combs)):
+# 					if(decision(0.5)):
+# 						templist = combs[k] 			
+# 						templist.append(j-1)
+# 						# print(templist)
+# 						temptrain = []
+# 						for l in range(len(templist)):
+# 							final_index = Yindexing[i] + templist[l]
+# 							temptrain += list(Xfull[final_index])
+# 						Xtrain_neg.append(temptrain)
+# 						Ytrain_neg.append(Y[i][j])
 
 
 
+# Xtrain = Xtrain_pos[0:2500] + Xtrain_neg[0:2500]
+# Ytrain = Ytrain_pos[0:2500] + Ytrain_neg[0:2500]
+# np.save("dataXtrain", Xtrain)
+# np.save("dataYtrain", Ytrain)
 
-
-Xtrain_pos = np.array(Xtrain_pos)
-Ytrain_pos = np.array(Ytrain_pos)
-Xtrain_neg = np.array(Xtrain_neg)
-Ytrain_neg = np.array(Ytrain_neg)
-
-
-np.save("dataXtrain_pos", Xtrain_pos)
-np.save("dataYtrain_pos", Ytrain_pos)
-np.save("dataXtrain_neg", Xtrain_neg)
-np.save("dataYtrain_neg", Ytrain_neg)
+# Xtrain = np.load("dataXtrain.npy")
+# Ytrain = np.load("dataYtrain.npy")
+# print(Xtrain.shape)
+# print(Ytrain.shape)
 
 
 
-#Loading
+
+
+# Xtrain_pos = np.array(Xtrain_pos)
+# Ytrain_pos = np.array(Ytrain_pos)
+# Xtrain_neg = np.array(Xtrain_neg)
+# Ytrain_neg = np.array(Ytrain_neg)
+
+
+# np.save("dataXtrain_pos", Xtrain_pos)
+# np.save("dataYtrain_pos", Ytrain_pos)
+# np.save("dataXtrain_neg", Xtrain_neg)
+# np.save("dataYtrain_neg", Ytrain_neg)
+
+
+
+# #Loading
 Xtrain_pos = np.load("dataXtrain_pos.npy")
 Xtrain_neg = np.load("dataXtrain_neg.npy")
-print(Xtrain_pos.shape)
-print(Xtrain_neg.shape)
+# print(Xtrain_pos.shape)
+# print(Xtrain_neg.shape)
 
 
 
 Ytrain_pos = np.load("dataYtrain_pos.npy")
 Ytrain_neg = np.load("dataYtrain_neg.npy")
-print(Ytrain_pos.shape)
-print(Ytrain_neg.shape)
+# print(Ytrain_pos.shape)
+# print(Ytrain_neg.shape)
+
+Xtrain_pos = list(Xtrain_pos)
+Xtrain_neg = list(Xtrain_neg)
+Ytrain_pos = list(Ytrain_pos) 
+Ytrain_neg = list(Ytrain_neg) 
 
 
-
-
+Xtrain = Xtrain_pos[0:200000] + Xtrain_neg[0:200000]
+Ytrain = Ytrain_pos[0:200000] + Ytrain_neg[0:200000]
+np.save("dataXtrain", Xtrain)
+np.save("dataYtrain", Ytrain)
 
 
 ##########################################################################
@@ -244,55 +251,55 @@ print("Postive in train: ", pos)
 print("Negative in train: ", neg)
 print("Total in train: ", len(Ytrain))
 
-clf = svm.SVC(gamma='auto', kernel='linear')
+clf = svm.LinearSVC(random_state=0, tol=1e-5, max_iter=10000)
 clf.fit(Xtrain, Ytrain)
 
 ##########################################################################
 #TAKING INPUT OF VALIDATION SET
 
-valX = []        
-valY = []
+# valX = []        
+# valY = []
 
-c=0
-for folder in foldersval:
-	c+=1
-	if(c%1000==0):
-		print("Input of Validation data: ", c)
-	imagesval_list = []
-	reading_images = []
-	tempo = []
-	for f in sorted(glob.glob(folder+"/*.png")):
-		imagesval_list.append(f)
+# c=0
+# for folder in foldersval:
+# 	c+=1
+# 	if(c%1000==0):
+# 		print("Input of Validation data: ", c)
+# 	imagesval_list = []
+# 	reading_images = []
+# 	tempo = []
+# 	for f in sorted(glob.glob(folder+"/*.png")):
+# 		imagesval_list.append(f)
 
-	for image in imagesval_list:
-		img  = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-		reading_images.append(img)
+# 	for image in imagesval_list:
+# 		img  = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+# 		reading_images.append(img)
 
-	for i in range(len(reading_images)):
-		temp = np.array(reading_images[i])
-		tempimg = temp.flatten()
-		tempimg = tempimg.reshape(33600, 1)
-		tempimg = tempimg.transpose()
-		tempimg = pca.transform(tempimg)
-		tempimg = tempimg.reshape(50)
-		tempo += list(tempimg)
+# 	for i in range(len(reading_images)):
+# 		temp = np.array(reading_images[i])
+# 		tempimg = temp.flatten()
+# 		tempimg = tempimg.reshape(33600, 1)
+# 		tempimg = tempimg.transpose()
+# 		tempimg = pca.transform(tempimg)
+# 		tempimg = tempimg.reshape(50)
+# 		tempo += list(tempimg)
 
-	# if(len(tempo)!=5):
-	# 	print(folder)
-	# print(len(tempo))
-	valX.append(tempo)
+# 	# if(len(tempo)!=5):
+# 	# 	print(folder)
+# 	# print(len(tempo))
+# 	valX.append(tempo)
 
 
-with open("./rewardsval.csv") as fileX:
-	x_reader = csv.reader(fileX)
-	for row in x_reader:
-		valY.append(int(float(row[1])))
+# with open("./rewardsval.csv") as fileX:
+# 	x_reader = csv.reader(fileX)
+# 	for row in x_reader:
+# 		valY.append(int(float(row[1])))
 
-valX = np.array(valX)
-valY = np.array(valY)
+# valX = np.array(valX)
+# valY = np.array(valY)
 
-np.save("datavalX", valX)
-np.save("datavalY", valY)
+# np.save("datavalX", valX)
+# np.save("datavalY", valY)
 
 #Loading
 valX = np.load("datavalX.npy")
@@ -330,6 +337,8 @@ print("Accuracy: ", accuracy)
 
 f1 = f1_score(valY, Ypredval, average=None) 
 print(f1)
+
+print("F1 Score: ", np.mean(f1))
 
 confusion = confusion_matrix(valY, Ypredval)
 print(confusion)
