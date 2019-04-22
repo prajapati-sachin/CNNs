7cd817ead39b1a99236279e04ad55730798f09a1
Sachin Kumar Prajapati <sachinkp1211@gmail.com>
2019-04-22 20:50:39 +0530

    modified CNN.py

--- cnn.py
+++ cnn.py
@@ -11,12 +11,14 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+import keras
+from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatte
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 
-folders50 = sorted(glob.glob("../train2/*"))
-foldersval = sorted(glob.glob("../validation/*"))
-foldersfull = sorted(glob.glob("../train5/*"))
+folders50 = sorted(glob.glob("./train2/*"))
+foldersval = sorted(glob.glob("./validation/*"))
+foldersfull = sorted(glob.glob("./train5/*"))
 
 
 ##########################################################################
@@ -57,56 +59,56 @@ foldersfull = sorted(glob.glob("../train5/*"))
 ##########################################################################
 #TAKING INPUT OF ALL IMAGES AND REDUCING DIMENSION AT SAME TIME
 
-images_list = []
-reward_list = []
-for folder in foldersfull:
-    for f in sorted(glob.glob(folder+"/*.png")):
-        images_list.append(f)
-    reward_list.append(folder+"/rew.csv")
+# images_list = []
+# reward_list = []
+# for folder in foldersfull:
+#     for f in sorted(glob.glob(folder+"/*.png")):
+#         images_list.append(f)
+#     reward_list.append(folder+"/rew.csv")
 
-Xfull = []
+# Xfull = []
 
-# read_images = []        
-c=0
-for image in images_list:
-    c+=1
-    if(c%2000==0):
-    	print("Input of full data: ", c/2000)
-    img = cv2.imread(image)
-    temp = np.array(img)
-    # print(temp.shape)
-    # temp = temp.flatten()
-    # temp = temp.reshape(33600, 1)
-    # temp = temp.transpose()
-    # temp = pca.transform(temp)
-    # temp = temp.reshape(50)
-    Xfull.append(list(temp))
+# # read_images = []        
+# c=0
+# for image in images_list:
+#     c+=1
+#     if(c%2000==0):
+#     	print("Input of full data: ", c/2000)
+#     img = cv2.imread(image)
+#     temp = np.array(img)
+#     # print(temp.shape)
+#     # temp = temp.flatten()
+#     # temp = temp.reshape(33600, 1)
+#     # temp = temp.transpose()
+#     # temp = pca.transform(temp)
+#     # temp = temp.reshape(50)
+#     Xfull.append(list(temp))
 
-Xfull = np.array(Xfull)
-print(Xfull.shape)
+# Xfull = np.array(Xfull)
+# print(Xfull.shape)
 
-np.save("dataXfull", Xfull)
+# np.save("dataXfull", Xfull)
 
-########### Input Y #########
-Y = []
+# ########### Input Y #########
+# Y = []
 
-for rewards in reward_list:
-	temp = []
-	with open(rewards) as fileX:
-		x_reader = csv.reader(fileX)
-		for row in x_reader:
-			temp.append(int(float(row[0])))
-		Y.append(temp)
+# for rewards in reward_list:
+# 	temp = []
+# 	with open(rewards) as fileX:
+# 		x_reader = csv.reader(fileX)
+# 		for row in x_reader:
+# 			temp.append(int(float(row[0])))
+# 		Y.append(temp)
 
-Y = np.array(Y)
-np.save("dataYfull", Y)
+# Y = np.array(Y)
+# np.save("dataYfull", Y)
 # #############################
 
 #Loading
-# Xfull = np.load("dataXfull.npy")
+# Xfull = np.load("../sachin/xc15.npy")
 # print(Xfull.shape)
 
-# Y = np.load("dataYfull.npy")
+# Y = np.load("../sachin/yc15.npy")
 # print(Y.shape)
 
 ##########################################################################
@@ -132,90 +134,106 @@ def getcomb(j):
 		result.append(list(temp1[i]))
 	return result
 
+def f1score(ytrue, ypred):
+	return np.mean(f1_score(ytrue, ypred, average=None))
+
+
+
 ##########################################################################
 #GENERATING TRAINING DATA
 
-Xtrain = []
-Ytrain = []
+# Xtrain = []
+# Ytrain = []
 
 
-Yindexing = [None]*len(Y)
-Yindexing[0] = 0
+# Yindexing = [None]*len(Y)
+# Yindexing[0] = 0
 
-for i in range(1, len(Y)):
-	Yindexing[i] = Yindexing[i-1] + len(Y[i-1])+1
+# for i in range(1, len(Y)):
+# 	Yindexing[i] = Yindexing[i-1] + len(Y[i-1])+1
 
-print(Yindexing)
-
-
-for i in range(len(Yindexing)):
-	num_images = len(Y[i])+1
-	for j in range(7, len(Y[i])):
-		if(Y[i][j]==1):
-			combs = getcomb(j)
-			for k in range(len(combs)):
-				templist = combs[k] 			
-				templist.append(j-1)
-				# print(templist)
-				temptrain = np.empty([210, 160, 3])
-				for l in range(len(templist)):
-					final_index = Yindexing[i] + templist[l]
-					# temptrain += list(Xfull[final_index])
-					# print(temptrain.shape)
-					# print(Xfull[final_index].shape)
-					np.concatenate((temptrain, Xfull[final_index]), axis=2)
-				Xtrain.append(temptrain)
-				Ytrain.append(1)
-		else:
-			if(decision(0.15)):
-				combs = getcomb(j)
-				for k in range(len(combs)):
-					if(decision(0.5)):
-						templist = combs[k] 			
-						templist.append(j-1)
-						# print(templist)
-						# temptrain = []
-						temptrain = np.empty([210, 160, 3])
-						for l in range(len(templist)):
-							final_index = Yindexing[i] + templist[l]
-							# temptrain += list(Xfull[final_index])
-							# print(temptrain.shape)
-							# print(Xfull[final_index].shape)
-							np.concatenate((temptrain, Xfull[final_index]), axis=2)
-						Xtrain.append(temptrain)
-						Ytrain.append(Y[i][j])
+# # print(Yindexing)
 
 
-
-
-Xtrain = np.array(Xtrain)
-Ytrain = np.array(Ytrain)
-
-np.save("dataXtrain", Xtrain)
-np.save("dataYtrain", Ytrain)
-
-#Loading
-Xtrain = np.load("dataXtrain.npy")
-print(Xtrain.shape)
-
-Ytrain = np.load("dataYtrain.npy")
-print(Ytrain.shape)
-
-pos = 0
-neg = 0
-for i in range(len(Ytrain)):
-	if(Ytrain[i]==0):
-		neg+=1
-	elif(Ytrain[i]==1):
-		pos+=1
-	else:
-		print("Problem")
+# for i in range(len(Yindexing)):
+# 	num_images = len(Y[i])+1
+# 	for j in range(7, len(Y[i])):
+# 		if(Y[i][j]==1):
+# 			if(decision(0.4)):
+# 				combs = getcomb(j)
+# 				for k in range(len(combs)):
+# 					if(decision(0.333)):
+# 						templist = combs[k] 			
+# 						templist.append(j-1)
+# 						# print(templist)
+# 						# temptrain = []
+# 						temptrain = Xfull[Yindexing[i] + templist[0]]						
+# 						# temptrain = np.empty([210, 160, 3])
+# 						for l in range(1, len(templist)):
+# 							final_index = Yindexing[i] + templist[l]
+# 							# temptrain.append(list(Xfull[final_index]))
+# 							# print(temptrain.shape)
+# 							# print(Xfull[final_index].shape)
+# 							temptrain = np.append(temptrain, Xfull[final_index], axis=2)
+# 							# print(temptrain.shape)
+# 						# print(temptrain.shape)
+# 						Xtrain.append(list(temptrain))
+# 						Ytrain.append(1)
+# 		else:
+# 			if(decision(0.012)):
+# 				combs = getcomb(j)
+# 				for k in range(len(combs)):
+# 					if(decision(0.333)):
+# 						templist = combs[k] 			
+# 						templist.append(j-1)
+# 						# print(templist)
+# 						# temptrain = []
+# 						temptrain = Xfull[Yindexing[i] + templist[0]]
+# 						# print(temptrain.shape)
+# 						# print(type(temptrain))
+# 						# temptrain = np.empty([210, 160, 3])
+# 						for l in range(1, len(templist)):
+# 							final_index = Yindexing[i] + templist[l]
+# 							# temptrain.append(list(Xfull[final_index]))
+# 							# print(temptrain.shape)
+# 							# print(Xfull[final_index].shape)
+# 							temptrain = np.append(temptrain, Xfull[final_index], axis=2)
+# 							# print(temptrain.shape)							
+# 						# print(temptrain.shape)
+# 						Xtrain.append(list(temptrain))
+# 						Ytrain.append(Y[i][j])
 
 
 
-print("Postive in train: ", pos)
-print("Negative in train: ", neg)
-print("Total in train: ", len(Ytrain))
+
+# Xtrain = np.array(Xtrain)
+# Ytrain = np.array(Ytrain)
+
+# np.save("Xtrain15_5", Xtrain)
+# np.save("Ytrain15_5", Ytrain)
+
+# # Loading
+# # Xtrain = np.load("Xtrain_2.npy")
+# print(Xtrain.shape)
+
+# # Ytrain = np.load("Ytrain_2.npy")
+# print(Ytrain.shape)
+
+# pos = 0
+# neg = 0
+# for i in range(len(Ytrain)):
+# 	if(Ytrain[i]==0):
+# 		neg+=1
+# 	elif(Ytrain[i]==1):
+# 		pos+=1
+# 	else:
+# 		print("Problem")
+
+
+
+# print("Postive in train: ", pos)
+# print("Negative in train: ", neg)
+# print("Total in train: ", len(Ytrain))
 
 ##########################################################################
 #TRAINING SVM
@@ -226,46 +244,49 @@ print("Total in train: ", len(Ytrain))
 ##########################################################################
 #TAKING INPUT OF VALIDATION SET
 
-valX = []        
-valY = []
+# valX = []        
+# valY = []
 
-c=0
-for folder in foldersval:
-	c+=1
-	if(c%1000==0):
-		print("Input of Validation data: ", c)
-	imagesval_list = []
-	reading_images = []
-	tempo = np.empty([210, 160, 3])
-	for f in sorted(glob.glob(folder+"/*.png")):
-		imagesval_list.append(f)
+# c=0
+# for folder in foldersval:
+# 	c+=1
+# 	if(c%1000==0):
+# 		print("Input of Validation data: ", c)
+# 	imagesval_list = []
+# 	reading_images = []
+# 	for f in sorted(glob.glob(folder+"/*.png")):
+# 		imagesval_list.append(f)
 
-	for image in imagesval_list:
-		img  = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-		reading_images.append(img)
+# 	for image in imagesval_list:
+# 		img  = cv2.imread(image)
+# 		reading_images.append(img)
 
-	for i in range(len(reading_images)):
-		temp = np.array(reading_images[i])
-		np.concatenate((tempo, temp), axis=2)
+# 	# tempo = []
+# 	tempo = np.array(reading_images[0])
+# 	for i in range(1, len(reading_images)):
+# 		temp = np.array(reading_images[i])
+# 		# tempo.append(list(temp))
+# 		tempo = np.append(tempo, temp, axis=2)
 
-	# if(len(tempo)!=5):
-	# 	print(folder)
-	# print(len(tempo))
-	valX.append(list(tempo))
+# 	# if(len(tempo)!=5):
+# 	# 	print(folder)
+# 	# print(len(tempo))
+# 	# print(tempo.shape)
+# 	valX.append(list(tempo))
 
 
-with open("./rewardsval.csv") as fileX:
-	x_reader = csv.reader(fileX)
-	for row in x_reader:
-		valY.append(int(float(row[1])))
+# with open("./rewardsval.csv") as fileX:
+# 	x_reader = csv.reader(fileX)
+# 	for row in x_reader:
+# 		valY.append(int(float(row[1])))
 
-valX = np.array(valX)
-valY = np.array(valY)
+# valX = np.array(valX)
+# valY = np.array(valY)
 
-np.save("datavalX", valX)
-np.save("datavalY", valY)
+# np.save("datavalX", valX)
+# np.save("datavalY", valY)
 
-Loading
+# # Loading
 valX = np.load("datavalX.npy")
 print(valX.shape)
 
@@ -285,9 +306,9 @@ for i in range(len(valY)):
 
 
 
-print("Postive in val: ", pos)
-print("Negative in val: ", neg)
-print("Total in val: ", len(valY))
+# print("Postive in val: ", pos)
+# print("Negative in val: ", neg)
+# print("Total in val: ", len(valY))
 
 
 
@@ -304,25 +325,81 @@ print("Total in val: ", len(valY))
 
 # confusion = confusion_matrix(valY, Ypredval)
 # print(confusion)
+##########################################################################
+#DATA GENERATOR
+
+num_batches = 864
+batch_size = 128
+# num_batches = 864
+
+class DataGenrator(keras.utils.Sequence):
+	def __init__(self,  dim=(210, 160 ,15), batch_size =128 , shuffle=True):
+		self.dim = dim
+		self.shuffle = shuffle
+		self.on_epoch_end()
+
+	def __len__(self):
+		return num_batches
+
+	def __getitem__(self, index):
+		x = np.load("batches1/X_" + str(index) + ".npy")
+		y = np.load("batches1/Y_" + str(index) + ".npy")
+		return x, y
+
+	def on_epoch_end(self):
+		self.indexes = np.arange(num_batches * batch_size)
+		if(self.shuffle):
+			np.random.shuffle(self.indexes)
+
+
+
+
+
 
 ##########################################################################
 #MAKING THE ARCHITECTURE
 
 
 #create model
+# model = Sequential()
+
+# #add model layers
+# # model.add(Conv3D(32, kernel_size=3, activation=’relu’, input_shape=(28,28,1)))
+
+# model.add(Conv3D(32, kernel_size = (3, 3, 15), activation=’relu’, strides=(2, 2), input_shape=(210, 160, 15)))
+# model.add(MaxPooling3D(pool_size=(2, 2, 15), strides=(2, 2)))
+
+# model.add(Conv3D(64, kernel_size = (3, 3, 15), activation=’relu’,  strides=(2, 2), input_shape=(210, 160, 15)))
+# model.add(MaxPooling3D(pool_size=(2, 2, 15), strides=(2, 2)))
+
+# model.add(Dense(1024))
+
+
+# #compile model using accuracy to measure model performance
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+
+#Manish model
+
+# def get_model():
 model = Sequential()
+model.add(Conv2D(32, (3,3), strides=2, activation='relu', input_shape = (210,160,15)))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2))
 
-#add model layers
-# model.add(Conv3D(32, kernel_size=3, activation=’relu’, input_shape=(28,28,1)))
+model.add(Conv2D(64, (3,3), strides=2, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2))
 
-model.add(Conv3D(32, kernel_size = (3, 3, 15), activation=’relu’, strides=(2, 2), input_shape=(210, 160, 15)))
-model.add(MaxPooling3D(pool_size=(2, 2, 15), strides=(2, 2)))
+model.add(Flatten())
+model.add(Dense(2048, activation='relu'))
 
-model.add(Conv3D(64, kernel_size = (3, 3, 15), activation=’relu’,  strides=(2, 2), input_shape=(210, 160, 15)))
-model.add(MaxPooling3D(pool_size=(2, 2, 15), strides=(2, 2)))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+# 	return model
 
-model.add(Dense(1024))
+training_generator = DataGenrator(128)
 
+epochs = 20
 
-#compile model using accuracy to measure model performance
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit_generator(generator=training_generator, epochs = epochs, validation_data=(valX, valY), use_multiprocessing=True, workers=25)
+
